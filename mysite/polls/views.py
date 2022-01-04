3536425fab92b21624a3d30d3f234cfd0d53bcb5
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2022-01-04 12:11:08 +0530

    Index view added

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+def index(request):
+    return HttpResponse("hello my name is Nisal. this is my second django practice project. And I am going to create poll application")
