16b179498b27980250363e6e3d5c1490c01a5ad1
nisalrenuja <nrenuja@gmail.com>
2022-01-05 17:21:24 +0530

    Detail html added

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404,render
 from django.http import HttpResponse, response
 from .models import Question
+from django.template import context, loader
+from django.http import Http404
+
+
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ','.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
-def detail(request,question_id):
-    return HttpResponse("You are looking at question %s."%question_id)
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request,question_id):
     response = "You're looking at results of the question %s."
