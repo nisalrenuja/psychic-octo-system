ae4c568151c8fc3c052e3dcbe8dbaacb95d2f911
nisalrenuja <nrenuja@gmail.com>
2022-01-05 16:17:27 +0530

    Index html added

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse, response
-
+from .models import Question
 
 def index(request):
-    return HttpResponse("hello my name is Nisal.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ','.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 def detail(request,question_id):
     return HttpResponse("You are looking at question %s."%question_id)
